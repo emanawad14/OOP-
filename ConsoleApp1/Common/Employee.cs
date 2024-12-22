b1aafce0b3a70b3d93e425ab1263e062ff4049e1
emanawad14 <emanelnady2001@gmail.com>
2024-12-22 17:43:15 +0200

    Add files via upload
    oop

---
+++ ConsoleApp1/Common/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+
+
+
+
+
+    public class Hire_Data
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public Hire_Data(int _year, int _month, int _day)
+        {
+            Year = _year;
+            Month = _month;
+            Day = _day;
+        }
+        public override string ToString()
+        {
+            return $" Year : {Year} \n Month : {Month} \n Day :{Day}";
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// /////////////////////////////////////////////////
+    /// </summary>
+    public enum security
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public security SecurityLevel { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? gender_field { get; set; }
+        public Employee(Guid _id, string _name, security _security, int _salary, DateTime _data, string _gender)
+        {
+            Id = _id;
+            Name = _name;
+            SecurityLevel = _security;
+            Salary = _salary;
+            DateTime = _data;
+            gender_field = _gender;
+
+        }
+
+        public override string ToString()
+        {
+            return $" ID :{Id} \n Name :{Name} \n SecurityLevel :{SecurityLevel} \n Datatime :{DateTime} \n gender_field :{gender_field} ";
+        }
+    }
+}
+   
+
+    
+
+   
+
+
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+using Common;
+
+#region Employee
+Employee employee1 = new Employee(Guid.NewGuid(), "Eman", security.Guest, 14000, DateTime.Now, "Female");
+Employee employee2 = new Employee(Guid.NewGuid(), "Ahmed", security.DBA, 16000, DateTime.Now, "Male");
+Employee employee3 = new Employee(Guid.NewGuid(), "Aya", security.Developer, 25000, DateTime.Now, "Female");
+
+Console.WriteLine(employee1);
+Console.WriteLine(employee2);
+Console.WriteLine(employee3);
+#endregion
+
+
+
+Console.WriteLine(" *********************Data ****************************");
+Console.WriteLine(" *********************Data ****************************");
+Console.WriteLine(" *********************Data ****************************");
+Console.WriteLine(" *********************Data ****************************");
+Console.WriteLine(" *********************Data ****************************");
+Console.WriteLine(" *********************Data ****************************");
+
+
+Hire_Data Data =new Hire_Data(2001 ,8 ,14);
+Console.WriteLine(Data);
+
+
+
+
